5c0d8e0773a785442975b38ad9fa5efad3861cbb
seasea128 <seasea@windowslive.com>
2024-03-05 19:10:18 +0000

    Formatting + add removeItem to ModelPartList

--- source/main.cpp
+++ source/main.cpp
@@ -1,20 +1,19 @@
 #include "mainwindow.h"
-#include <QFile>
 #include <QApplication>
+#include <QFile>
 #include <iostream>
 
-int main(int argc, char *argv[])
-{
-    // Open Stylesheet file
-    QFile styleSheetFile("./Combinear.qss");
-    styleSheetFile.open(QFile::ReadOnly);
-    // Store all the contents of the file in a string
-    QString styleSheet = QLatin1String(styleSheetFile.readAll());
-    // Load the stylesheet
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.setStyleSheet(styleSheet);
-    w.show();
+int main(int argc, char *argv[]) {
+  // Open Stylesheet file
+  QFile styleSheetFile("./Combinear.qss");
+  styleSheetFile.open(QFile::ReadOnly);
+  // Store all the contents of the file in a string
+  QString styleSheet = QLatin1String(styleSheetFile.readAll());
+  // Load the stylesheet
+  QApplication a(argc, argv);
+  MainWindow w;
+  w.setStyleSheet(styleSheet);
+  w.show();
 
-    return a.exec();
+  return a.exec();
 }
