307118c7e9bb13b29749ce4234f3117bd1df1104
seasea128 <seasea@windowslive.com>
2024-04-08 18:55:12 +0100

    Formatting whole codebase with clang-format

--- source/RenderThread/Callback.h
+++ source/RenderThread/Callback.h
@@ -8,58 +8,55 @@
 #include <vtkRenderWindowInteractor.h>
 #include <vtkRendererCollection.h>
 
-class vtkTimerCallback : public vtkCallbackCommand
-{
-  public:
-    vtkTimerCallback() = default;
+class vtkTimerCallback : public vtkCallbackCommand {
+public:
+  vtkTimerCallback() = default;
 
-    static vtkTimerCallback *New(RenderThread *thread)
-    {
-        vtkTimerCallback *cb = new vtkTimerCallback;
-        cb->renderThread = thread;
-        return cb;
+  static vtkTimerCallback *New(RenderThread *thread) {
+    vtkTimerCallback *cb = new vtkTimerCallback;
+    cb->renderThread = thread;
+    return cb;
+  }
+
+  virtual void Execute(vtkObject *caller, unsigned long eventId,
+                       void *vtkNotUsed(callData)) {
+    if (vtkCommand::TimerEvent == eventId) {
+      auto iren = dynamic_cast<vtkRenderWindowInteractor *>(caller);
+
+      // This is where the logic for updating actors and thing should be
+      vtkActorCollection *actorList = iren->GetRenderWindow()
+                                          ->GetRenderers()
+                                          ->GetFirstRenderer()
+                                          ->GetActors();
+      vtkActor *a;
+
+      /* X Rotation */
+      actorList->InitTraversal();
+      while ((a = (vtkActor *)actorList->GetNextActor())) {
+        a->RotateX(renderThread->rotateX);
+      }
+
+      /* Y Rotation */
+      actorList->InitTraversal();
+      while ((a = (vtkActor *)actorList->GetNextActor())) {
+        a->RotateY(renderThread->rotateY);
+      }
+
+      /* Z Rotation */
+      actorList->InitTraversal();
+      while ((a = (vtkActor *)actorList->GetNextActor())) {
+        a->RotateZ(renderThread->rotateZ);
+      }
+
+      // Rerender the window whenever it's updated
+      if (renderThread->reRender) {
+        renderThread->window->Render();
+        renderThread->reRender = false;
+      }
     }
+  }
 
-    virtual void Execute(vtkObject *caller, unsigned long eventId, void *vtkNotUsed(callData))
-    {
-        if (vtkCommand::TimerEvent == eventId)
-        {
-            auto iren = dynamic_cast<vtkRenderWindowInteractor *>(caller);
-
-            // This is where the logic for updating actors and thing should be
-            vtkActorCollection *actorList = iren->GetRenderWindow()->GetRenderers()->GetFirstRenderer()->GetActors();
-            vtkActor *a;
-
-            /* X Rotation */
-            actorList->InitTraversal();
-            while ((a = (vtkActor *)actorList->GetNextActor()))
-            {
-                a->RotateX(renderThread->rotateX);
-            }
-
-            /* Y Rotation */
-            actorList->InitTraversal();
-            while ((a = (vtkActor *)actorList->GetNextActor()))
-            {
-                a->RotateY(renderThread->rotateY);
-            }
-
-            /* Z Rotation */
-            actorList->InitTraversal();
-            while ((a = (vtkActor *)actorList->GetNextActor()))
-            {
-                a->RotateZ(renderThread->rotateZ);
-            }
-
-            // Rerender the window whenever it's updated
-            if (renderThread->reRender) {
-                renderThread->window->Render();
-                renderThread->reRender = false;
-            }
-        }
-    }
-
-  private:
-    RenderThread *renderThread;
+private:
+  RenderThread *renderThread;
 };
 #endif // CALLBACK_H_
--- source/RenderThread/RenderThread.cpp
+++ source/RenderThread/RenderThread.cpp
@@ -4,136 +4,129 @@
 #include <vtkNamedColors.h>
 #include <vtkSmartPointer.h>
 
-RenderThread::RenderThread(QObject *parent, vtkSmartPointer<vtkRenderer> renderer,
-                           vtkSmartPointer<vtkRenderWindow> window,
-                           vtkSmartPointer<vtkRenderWindowInteractor> interactor, vtkSmartPointer<vtkCamera> camera)
-{
-    /* Initialise actor list */
-    actors = vtkActorCollection::New();
+RenderThread::RenderThread(
+    QObject *parent, vtkSmartPointer<vtkRenderer> renderer,
+    vtkSmartPointer<vtkRenderWindow> window,
+    vtkSmartPointer<vtkRenderWindowInteractor> interactor,
+    vtkSmartPointer<vtkCamera> camera) {
+  /* Initialise actor list */
+  actors = vtkActorCollection::New();
 
-    /* Initialise command variables */
-    rotateX = 0.;
-    rotateY = 0.;
-    rotateZ = 0.;
+  /* Initialise command variables */
+  rotateX = 0.;
+  rotateY = 0.;
+  rotateZ = 0.;
 
-    callback = vtkTimerCallback::New(this);
+  callback = vtkTimerCallback::New(this);
 
-    this->renderer = renderer;
-    this->window = window;
-    this->interactor = interactor;
-    this->camera = camera;
+  this->renderer = renderer;
+  this->window = window;
+  this->interactor = interactor;
+  this->camera = camera;
 }
 
-void RenderThread::run()
-{
-    /* You might want to edit the 3D model once VR has started, however VTK is not
-     * "thread safe". This means if you try to edit the VR model from the GUI
-     * thread while the VR thread is running, the program could become corrupted
-     * and crash. The solution is to get the VR thread to edit the model. Any
-     * decision to change the VR model will come fromthe user via the GUI thread,
-     * so there needs to be a mechanism to pass data from the GUi thread to the VR
-     * thread.
+void RenderThread::run() {
+  /* You might want to edit the 3D model once VR has started, however VTK is not
+   * "thread safe". This means if you try to edit the VR model from the GUI
+   * thread while the VR thread is running, the program could become corrupted
+   * and crash. The solution is to get the VR thread to edit the model. Any
+   * decision to change the VR model will come fromthe user via the GUI thread,
+   * so there needs to be a mechanism to pass data from the GUi thread to the VR
+   * thread.
+   */
+
+  vtkNew<vtkNamedColors> colors;
+
+  // Set the background color.
+  std::array<unsigned char, 4> bkg{{26, 51, 102, 255}};
+  colors->SetColor("BkgColor", bkg.data());
+
+  renderer->SetBackground(colors->GetColor3d("BkgColor").GetData());
+
+  /* Loop through list of actors provided and add to scene */
+  // vtkSmartPointer<vtkCompositePolyDataMapper> bigMapper =
+  // vtkCompositePolyDataMapper::New();
+  vtkActor *a;
+  // vtkMapper *m;
+  // mappers->InitTraversal();
+  // while ((m = (vtkMapper *)mappers->GetNextItem()))
+  //{
+  // }
+  actors->InitTraversal();
+  while ((a = (vtkActor *)actors->GetNextActor())) {
+    renderer->AddActor(a);
+  }
+
+  // window->Initialize();
+  window->AddRenderer(renderer);
+
+  renderer->SetActiveCamera(camera);
+
+  interactor->SetRenderWindow(window);
+  interactor->Initialize();
+
+  interactor->AddObserver(vtkCommand::TimerEvent, callback);
+  int timerId = interactor->CreateRepeatingTimer(20);
+
+  interactor->Start();
+  window->Render();
+}
+
+RenderThread::~RenderThread() {
+  vtkActor *a;
+  actors->InitTraversal();
+  while ((a = actors->GetNextActor())) {
+    a->Delete();
+  }
+  callback->Delete();
+}
+
+void RenderThread::addActorOffline(vtkActor *actor) {
+  if (!this->isRunning()) {
+    double *ac = actor->GetOrigin();
+
+    /* I have found that these initial transforms will position the FS
+     * car model in a sensible position but you can experiment
      */
+    actor->RotateX(-90);
+    actor->AddPosition(-ac[0] + 0, -ac[1] - 100, -ac[2] - 200);
 
-    vtkNew<vtkNamedColors> colors;
-
-    // Set the background color.
-    std::array<unsigned char, 4> bkg{{26, 51, 102, 255}};
-    colors->SetColor("BkgColor", bkg.data());
-
-    renderer->SetBackground(colors->GetColor3d("BkgColor").GetData());
-
-    /* Loop through list of actors provided and add to scene */
-    // vtkSmartPointer<vtkCompositePolyDataMapper> bigMapper = vtkCompositePolyDataMapper::New();
-    vtkActor *a;
-    // vtkMapper *m;
-    // mappers->InitTraversal();
-    // while ((m = (vtkMapper *)mappers->GetNextItem()))
-    //{
-    // }
-    actors->InitTraversal();
-    while ((a = (vtkActor *)actors->GetNextActor()))
-    {
-        renderer->AddActor(a);
-    }
-
-    // window->Initialize();
-    window->AddRenderer(renderer);
-
-    renderer->SetActiveCamera(camera);
-
-    interactor->SetRenderWindow(window);
-    interactor->Initialize();
-
-    interactor->AddObserver(vtkCommand::TimerEvent, callback);
-    int timerId = interactor->CreateRepeatingTimer(20);
-
-    interactor->Start();
-    window->Render();
+    actors->AddItem(actor);
+  }
 }
 
-RenderThread::~RenderThread()
-{
-    vtkActor *a;
-    actors->InitTraversal();
-    while ((a = actors->GetNextActor()))
-    {
-        a->Delete();
-    }
-    callback->Delete();
+void RenderThread::addMapperOffline(vtkMapper *mapper) {
+  if (!this->isRunning()) {
+    mappers->AddItem(mapper);
+  }
 }
 
-void RenderThread::addActorOffline(vtkActor *actor)
-{
-    if (!this->isRunning())
-    {
-        double *ac = actor->GetOrigin();
+void RenderThread::issueCommand(int cmd, double value) {
 
-        /* I have found that these initial transforms will position the FS
-         * car model in a sensible position but you can experiment
-         */
-        actor->RotateX(-90);
-        actor->AddPosition(-ac[0] + 0, -ac[1] - 100, -ac[2] - 200);
+  /* Update class variables according to command */
+  switch (cmd) {
+  /* These are just a few basic examples */
+  case END_RENDER:
+    this->endRender = true;
+    break;
 
-        actors->AddItem(actor);
-    }
-}
+  case ROTATE_X:
+    this->rotateX = value;
+    break;
 
-void RenderThread::addMapperOffline(vtkMapper *mapper)
-{
-    if (!this->isRunning())
-    {
-        mappers->AddItem(mapper);
-    }
-}
+  case ROTATE_Y:
+    this->rotateY = value;
+    break;
 
-void RenderThread::issueCommand(int cmd, double value)
-{
+  case ROTATE_Z:
+    this->rotateZ = value;
+    break;
 
-    /* Update class variables according to command */
-    switch (cmd)
-    {
-    /* These are just a few basic examples */
-    case END_RENDER:
-        this->endRender = true;
-        break;
-
-    case ROTATE_X:
-        this->rotateX = value;
-        break;
-
-    case ROTATE_Y:
-        this->rotateY = value;
-        break;
-
-    case ROTATE_Z:
-        this->rotateZ = value;
-        break;
-
-    case RE_RENDER:
-        // Cannot call Render() on renderWindow directly here, since it will be executed on main thread. It need to be
-        // called from renderThread itself. - Chanon Yothavut
-        this->reRender = true;
-        break;
-    }
+  case RE_RENDER:
+    // Cannot call Render() on renderWindow directly here, since it will be
+    // executed on main thread. It need to be called from renderThread itself. -
+    // Chanon Yothavut
+    this->reRender = true;
+    break;
+  }
 }
