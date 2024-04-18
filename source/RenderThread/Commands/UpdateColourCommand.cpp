1b9c1089e6a6972dec0a3f4c2f9314c1bbb92cb7
seasea128 <seasea@windowslive.com>
2024-04-18 05:29:56 +0100

    Change how UpdateColourCommand is constructed

--- source/RenderThread/Commands/UpdateColourCommand.cpp
+++ source/RenderThread/Commands/UpdateColourCommand.cpp
@@ -2,12 +2,30 @@
 
 #include "../RenderThread.h"
 
-UpdateColourCommand::UpdateColourCommand(vtkActor *_actorToUpdate,
-                                         vtkColor3<unsigned char> _updateColour)
-    : actorToUpdate(_actorToUpdate), updateColour(_updateColour) {}
+// UpdateColourCommand::UpdateColourCommand(vtkActor *_actorToUpdate,
+//                                          vtkColor3<unsigned char>
+//                                          _updateColour)
+//     : actorToUpdate(_actorToUpdate), updateColour(_updateColour) {}
+
+UpdateColourCommand::UpdateColourCommand(const ModelPart *part)
+    : actorToUpdate(part->getVRActor()), updateColour(part->getColour()) {}
 
 UpdateColourCommand::~UpdateColourCommand() {}
 
+UpdateColourCommand::UpdateColourCommand(const UpdateColourCommand &command)
+    : actorToUpdate(command.actorToUpdate), updateColour(command.updateColour) {
+}
+
 void UpdateColourCommand::Execute(RenderThread &renderThread) {
   renderThread.updateColour(actorToUpdate, updateColour);
+}
+
+UpdateColourCommand &
+UpdateColourCommand::operator=(const UpdateColourCommand &command) {
+  if (this == &command) {
+    return *this;
+  }
+  this->actorToUpdate = command.actorToUpdate;
+  this->updateColour = command.updateColour;
+  return *this;
 }
--- source/RenderThread/Commands/UpdateColourCommand.h
+++ source/RenderThread/Commands/UpdateColourCommand.h
@@ -1,15 +1,18 @@
 #ifndef UPDATECOLOURCOMMAND_H_
 #define UPDATECOLOURCOMMAND_H_
+#include "../../../ModelPart.h"
 #include "BaseCommand.h"
 #include <vtkActor.h>
 #include <vtkColor.h>
 
 class UpdateColourCommand : public BaseCommand {
-  using BaseCommand::operator=;
 
 public:
-  UpdateColourCommand(vtkActor *_actorToUpdate,
-                      vtkColor3<unsigned char> _updateColour);
+  // UpdateColourCommand(vtkActor *_actorToUpdate,
+  //                     vtkColor3<unsigned char> _updateColour);
+  UpdateColourCommand(const ModelPart *part);
+  UpdateColourCommand(const UpdateColourCommand &command);
+  UpdateColourCommand &operator=(const UpdateColourCommand &command);
   virtual ~UpdateColourCommand();
   virtual void Execute(RenderThread &renderThread) override;
 
