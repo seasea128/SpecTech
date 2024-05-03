abed2b1dcbfacc6e59d3cd7aea2a2ef71c843feb
seasea128 <seasea@windowslive.com>
2024-05-03 15:45:22 +0100

    Update UI again

--- source/optiondialog.cpp
+++ source/optiondialog.cpp
@@ -5,7 +5,7 @@
 #include <QPalette>
 
 OptionDialog::OptionDialog(QWidget *parent, ModelPart *part)
-    : QDialog(parent), ui(new Ui::OptionDialog) {
+    : QWidget(parent), ui(new Ui::OptionDialog) {
   ui->setupUi(this);
   this->part = part;
   ui->lineEdit->setText(part->data(0).toString());
@@ -22,19 +22,23 @@ OptionDialog::OptionDialog(QWidget *parent, ModelPart *part)
 
   float max = 100.;
 
-  qDebug() <<roughnessValue;
-  qDebug() << static_cast<int>(part->getRoughness()*max);
+  qDebug() << roughnessValue;
+  qDebug() << static_cast<int>(part->getRoughness() * max);
 
   ui->MetallicSlider->setValue(static_cast<int>(part->getMetallic() * max));
   ui->RoughnessSlider->setValue(static_cast<int>(part->getRoughness() * max));
   ui->AnisotropySlider->setValue(static_cast<int>(part->getAnisotropy() * max));
-  ui->AnisotropyRotation->setValue(static_cast<int>(part->getAnisotropyRotation() * max));
+  ui->AnisotropyRotation->setValue(
+      static_cast<int>(part->getAnisotropyRotation() * max));
 
-  connect(ui->RoughnessSlider, &QSlider::sliderMoved, this, &OptionDialog::on_RoughnessSlider_sliderMoved);
-  connect(ui->MetallicSlider, &QSlider::sliderMoved, this, &OptionDialog::on_MetallicSlider_sliderMoved);
-  connect(ui->AnisotropySlider, &QSlider::sliderMoved, this, &OptionDialog::on_AnisotropySlider_sliderMoved);
-  connect(ui->AnisotropyRotation, &QSlider::sliderMoved, this, &OptionDialog::on_AnisotropyRotation_sliderMoved);
-
+  connect(ui->RoughnessSlider, &QSlider::sliderMoved, this,
+          &OptionDialog::on_RoughnessSlider_sliderMoved);
+  connect(ui->MetallicSlider, &QSlider::sliderMoved, this,
+          &OptionDialog::on_MetallicSlider_sliderMoved);
+  connect(ui->AnisotropySlider, &QSlider::sliderMoved, this,
+          &OptionDialog::on_AnisotropySlider_sliderMoved);
+  connect(ui->AnisotropyRotation, &QSlider::sliderMoved, this,
+          &OptionDialog::on_AnisotropyRotation_sliderMoved);
 }
 
 OptionDialog::~OptionDialog() { delete ui; }
@@ -59,7 +63,6 @@ void OptionDialog::SetValue() {
   part->setMetallic(metallicValue);
   part->setAnisotropy(anisotropyValue);
   part->setAnisotropyRotation(anisotropyRotationValue);
-
 }
 
 void OptionDialog::on_ColourDialog_clicked() {
